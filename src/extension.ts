e6810289673911130488b4ec77b637dc1b6ce855
Петр Болф <petr.bolf@domogled.com>
2016-11-16 04:09:01 +0100

    const config = vscode.workspace.getConfiguration('pruga')

--- src/extension.ts
+++ src/extension.ts
@@ -9,9 +9,7 @@ import {HttpPreviewContentProvider} from './httpPreviewContentProvider'
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
     const rootPath = vscode.workspace.rootPath;
-    const config = require(path.join(rootPath, 'pruga.js'))
-    const SHOW_INFORMATION_MESSAGE = config["showInformationMessage"]
-    
+        
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "pruga-vscode-preview" is now active!');
@@ -20,22 +18,24 @@ export function activate(context: vscode.ExtensionContext) {
     const registration = vscode.workspace.registerTextDocumentContentProvider("http", provider);
 
 
-    const {protocol, host, port, dir, file} = config.web
-    const previewUri = `${protocol}://${host}:${port}/${dir}/${file}`
+    
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
     const disposable = vscode.commands.registerCommand('pruga.preview', () => {
-        // The code you place here will be executed every time your command is executed
+        const config = vscode.workspace.getConfiguration('pruga')
+        const SHOW_INFO = `${config.get('showLevel')}` == "info"
 
-
+        // const {protocol, host, port, dir, file} = config.web
+        // const previewUri = `${protocol}://${host}:${port}/${dir}/${file}`
+        const previewUri = config.get('server.web')
 
         /*let success = await*/ vscode.commands.executeCommand('vscode.previewHtml', previewUri, vscode.ViewColumn.Two)
             .then(
                 success => {
                     console.log(`Пруга preview: web will be preview: ${success}`)
-                    if(SHOW_INFORMATION_MESSAGE) {
+                    if(SHOW_INFO) {
                         vscode.window.showInformationMessage(`Пруга preview: web will be preview: ${success}`);
                     }
                     
